44bcf9df17b1a8ebd78c93eae885c997b1ee6f97
Meghana Holla <meghanaholla8526@gmail.com>
2025-07-01 11:43:13 +0530

    final changes

---
+++ src/test/java/Base.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.time.Duration;
+
+public class Base {
+    static WebDriver driver;
+    @BeforeSuite
+    public void openApplication(){
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+
+    @AfterSuite
+    public void closeApplication(){
+        driver.close();
+    }
+}
